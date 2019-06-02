72612cb8ab6ccb73fdfe8d9f0e8a3f34e7077a21
Yechiel K <contact@yechiel.me>
2019-06-02 11:13:23 -0400

    twitter reminder working

--- first-extension.js
+++ first-extension.js
@@ -1,1 +1,8 @@
-alert("Hello Codeland!")
+// intrval is in milliseconds, so 1000 is a second, times 6 is a minute, and times 10 for 10 minutes
+const interval = 10 * 60 * 1000
+
+function reminder() {
+    alert("Get off Twitter!")
+}
+
+setInterval(reminder, interval)

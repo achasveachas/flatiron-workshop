aa2956743f98375dac264d69132f501020732105
Yechiel K <contact@yechiel.me>
2019-06-02 21:14:41 -0400

    start work on unbiasify

--- first-extension.js
+++ first-extension.js
@@ -1,1 +1,4 @@
-alert("Hello Codeland!")
+let images = document.querySelectorAll('img[class*="EntityPhoto"]')
+for (i = 0; i < images.length; i++) {
+    images[i].src = browser.runtime.getURL("images/kitten.jpg")
+}  

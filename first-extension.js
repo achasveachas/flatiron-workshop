6c225f7c7b0b3eff156ed9251ee61204cf756335
Yechiel K <contact@yechiel.me>
2019-06-02 22:11:01 -0400

    got unbiasify working

--- first-extension.js
+++ first-extension.js
@@ -1,4 +1,16 @@
-let images = document.querySelectorAll('img[class*="EntityPhoto"]')
-for (i = 0; i < images.length; i++) {
-    images[i].src = browser.runtime.getURL("images/kitten.jpg")
-}  
+const callback = function(mutationsList, observer) {
+    let images = document.querySelectorAll('img[class*="EntityPhoto"]')
+
+    for (i = 0; i < images.length; i++) {
+        images[i].src = browser.runtime.getURL("images/kitten.jpg")
+    }
+}
+
+// Options for the observer (which mutations to observe)
+const config = { childList: true, subtree: true }
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback)
+
+// Start observing the target node for configured mutations
+observer.observe(document.body, config)
